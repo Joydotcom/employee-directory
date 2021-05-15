d20d21cfdc7953c6bda7e7252dd70f94df1a2948
joydotcom <msjoyjackson@gmail.com>
2021-05-14 23:12:20 -0400

    added header and wrapper components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+export default class header extends Component {
+    render() {
+        return (
+            <div className="header">
+            <h1> Employee Directory</h1>
+            <p> Click to flip through or search.</p>
+            </div>
+        )
+    }
+}
