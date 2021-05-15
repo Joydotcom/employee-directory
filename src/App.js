4625c1501932d2a81de7ea555a8ba217e92392c2
joydotcom <msjoyjackson@gmail.com>
2021-05-14 23:18:34 -0400

    added css to wrapper inside its own folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Main from "./components/Main";
-import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapper/Wrapper";
 import Header from "./components/Header";
 import logo from './logo.svg';
 import './App.css';
